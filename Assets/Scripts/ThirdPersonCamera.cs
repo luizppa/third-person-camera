883da6fa8a50eee5515d8c3bd65523713659545d
luizppa <luizphilippep@gmail.com>
2022-07-19 18:14:25 -0300

    :sparkles: Control settings

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -29,10 +29,19 @@ public class ThirdPersonCamera : MonoBehaviour
   [SerializeField] RingConfiguration middleRing = new RingConfiguration { radius = 2f, height = 1.4f, color = Color.red };
   [SerializeField] RingConfiguration bottomRing = new RingConfiguration { radius = 2f, height = 1.4f, color = Color.red };
 
+  [Header("Controls")]
+  [SerializeField] string horizontalAxis = "Mouse X";
+  [SerializeField] float horizontalSensibility = 1f;
+  [SerializeField] bool invertX = false;
+  [SerializeField] string verticalAxis = "Mouse Y";
+  [SerializeField] float verticalSensibility = 1f;
+  [SerializeField] bool invertY = true;
+
   [Header("Editor Settings")]
   [SerializeField] bool showGizmos = true;
 
   private float cameraTranslation = 0f;
+  private float verticalMultiplier = 10f;
 
   void Start()
   {
@@ -53,8 +62,17 @@ public class ThirdPersonCamera : MonoBehaviour
 
   private void SetPosition()
   {
-    Vector3 movement = follow.transform.up * Input.GetAxis("Mouse Y");
-    cameraTranslation += Input.GetAxis("Mouse X") * 10f;
+    Vector3 movement = follow.transform.up * Input.GetAxis(verticalAxis) * verticalSensibility * (invertY ? -1 : 1);
+    cameraTranslation += Input.GetAxis(horizontalAxis) * verticalMultiplier * horizontalSensibility * (invertX ? -1 : 1);
+
+    if (cameraTranslation > 360f)
+    {
+      cameraTranslation -= 360f;
+    }
+    else if (cameraTranslation < 0f)
+    {
+      cameraTranslation += 360f;
+    }
     Plane referencePlane = new Plane(follow.transform.up, follow.transform.position);
 
     float referenceHeight = referencePlane.GetDistanceToPoint(transform.position + movement);
@@ -72,7 +90,7 @@ public class ThirdPersonCamera : MonoBehaviour
 
   private RingConfiguration GetCameraRing(float referenceHeight)
   {
-
+    // TODO: Modify lerp to ease-in-out
     if (referenceHeight >= topRing.height)
     {
       return new RingConfiguration { radius = topRing.radius, height = topRing.height, color = Color.green };
@@ -98,6 +116,14 @@ public class ThirdPersonCamera : MonoBehaviour
   private void SetRotation()
   {
     transform.LookAt(lookAt.transform);
+    // float angle = Vector3.Angle(transform.up, follow.transform.up);
+    // float angleState = Mathf.InverseLerp(0f, 180f, cameraTranslation);
+    // if (cameraTranslation > 180f)
+    // {
+    //   angleState = Mathf.InverseLerp(360f, 180f, cameraTranslation);
+    // }
+    // float angle = Mathf.Lerp(follow.transform.eulerAngles.z, -follow.transform.eulerAngles.z, angleState);
+    // transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, angle);
   }
 
   private void InitRotation()
