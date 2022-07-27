3c1ca71cee43970f7341b869e8daa011a76bf60e
luizppa <luizphilippep@gmail.com>
2022-07-27 19:46:57 -0300

    :bug: Normalize player movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -27,8 +27,8 @@ public class PlayerControl : MonoBehaviour
 
   void Rotate()
   {
-    Vector3 direction = Vector3.ProjectOnPlane(GetMoveDirection(), transform.up);
-    if (direction.magnitude > 0.1f)
+    Vector3 direction = GetMoveDirection();
+    if (direction.magnitude > 0f)
     {
       transform.rotation = Quaternion.LookRotation(direction);
     }
@@ -36,8 +36,8 @@ public class PlayerControl : MonoBehaviour
 
   void Move()
   {
-    Vector3 direction = Vector3.ProjectOnPlane(GetMoveDirection(), transform.up);
-    if (direction.magnitude > 0.1f)
+    Vector3 direction = GetMoveDirection();
+    if (direction.magnitude > 0f)
     {
       Vector3 velocity = direction * GetMoveSpeed() * Time.deltaTime * 60f;
       rb.AddForce(velocity, ForceMode.VelocityChange);
@@ -47,6 +47,11 @@ public class PlayerControl : MonoBehaviour
   private Vector3 GetMoveDirection()
   {
     Vector3 direction = (gameCamera.transform.right * Input.GetAxis("Horizontal")) + (gameCamera.transform.forward * Input.GetAxis("Vertical"));
+    if (direction.magnitude <= 0.1f)
+    {
+      return Vector3.zero;
+    }
+    direction = Vector3.ProjectOnPlane(direction, transform.up).normalized;
     return direction;
   }
 
